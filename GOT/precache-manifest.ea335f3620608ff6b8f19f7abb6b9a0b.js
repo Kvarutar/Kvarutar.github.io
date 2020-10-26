e571803bf8f4b7d8c73ee903deadf64cb503eb4d
Kvarutar <48532279+Kvarutar@users.noreply.github.com>
2020-10-26 19:53:28 +0300

    Add files via upload

---
+++ GOT/precache-manifest.ea335f3620608ff6b8f19f7abb6b9a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d9390bb65fd8065e35c",
+    "url": "/static/css/main.afef8a0d.chunk.css"
+  },
+  {
+    "revision": "2d9390bb65fd8065e35c",
+    "url": "/static/js/main.2d9390bb.chunk.js"
+  },
+  {
+    "revision": "303a367b9cc2aa55f0d1",
+    "url": "/static/css/1.60c7b1bd.chunk.css"
+  },
+  {
+    "revision": "303a367b9cc2aa55f0d1",
+    "url": "/static/js/1.303a367b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1b49b2980fe32855e49912c9fade7c98",
+    "url": "/index.html"
+  }
+];
