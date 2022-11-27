2026847d5139c2d6266600be578582f32bb7222b
Kvarutar <v_aleks_s@mail.ru>
2022-11-27 21:37:46 +0300

    initial

---
+++ Hospital/js/index.js
@@ -0,0 +1,5 @@
+$('.news__slider').slick({
+    prevArrow:"<div class='slick-prev'><img class='a-left control-c prev' src='../icons/left.svg'></div>",
+    nextArrow:"<div class='slick-next'><img class='a-right control-c next' src='../icons/right.svg'></div>",
+    dots: true
+});
